f954a2d2ed2fad9aeebb910f7667c0f1446f73b2
Vanessa Fillis <71226151+vfillis@users.noreply.github.com>
2021-07-20 09:53:22 +0100

    Create start.js

---
+++ docs/javascript/start.js
@@ -0,0 +1,12 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.project-title');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.project-title .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  });
